7619b03d220542da438799f75e7f38fa5873463a
danesco <danesco30@gmail.com>
2018-10-01 23:14:35 +0000

    made getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
 console.log("Welcome to the Github avatar downloader!");
+
+function getRepoContributors(repoOwner, repoName, cb){
+
+};
+
+getRepoContributors("jquery", "jquery", function(err, result){
+  console.log('errors:', err);
+  console.log('results;', result);
+});
