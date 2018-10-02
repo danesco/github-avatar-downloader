e13238655306fc766724292193087df2a747be2f
danesco <danesco30@gmail.com>
2018-10-02 02:00:33 +0000

    finished the downloadImageByUrl function

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,8 @@
 var request = require('request');
-var token = require('./secrets.js')
+var token = require('./secrets.js');
+var fs = require('fs');
+var owner = process.argv[2];
+var repo = process.argv[3];
 
 console.log("Welcome to the Github avatar downloader!");
 
@@ -24,5 +27,19 @@ function getRepoContributors(repoOwner, repoName, cb){
 getRepoContributors("jquery", "jquery", function(err, result){
   for(var key in result){
     console.log(result[key].avatar_url);
+    downloadImageByURL(result[key].avatar_url, "avatars/" + result[key].login + ".png")
   }
 });
+
+
+function downloadImageByURL(url, filePath){
+  request.get(url)
+          .on("error", function(err){
+            throw err;
+          })
+          .on("response", function(response){
+            console.log("Status Code: ", response.statusCode);
+          })
+          .pipe(fs.createWriteStream(filePath));
+}
+
