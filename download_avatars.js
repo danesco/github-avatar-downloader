905c8a1adcf875c96a638e2d6ddefccd41a4dbb9
danesco <danesco30@gmail.com>
2018-10-02 02:10:48 +0000

    added conditional to make sure user is giving 2 command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -15,18 +15,22 @@ function getRepoContributors(owner, repo, cb){
       'Authorization': "token " + token.GITHUB_TOKEN
       }
   };
+
   //3 Request call is made
   request(options, function(err, res, body){
     //4 call the callback
-    var toObject = JSON.parse(body);
-    cb(err, toObject);
+    if (process.argv.length !== 4){
+      console.log("You must give 2 arguments")
+    }else{
+      var toObject = JSON.parse(body);
+      cb(err, toObject);
+    }
   })
 };
 
 //1 Starting point                      //5 callback coming from number 4
 getRepoContributors("jquery", "jquery", function(err, result){
   for(var key in result){
-    console.log(result[key].avatar_url);
     downloadImageByURL(result[key].avatar_url, "avatars/" + result[key].login + ".png")
   }
 });
